166761b4eaedd3ab560ced34f40f962c6b7840eb
Moshe Dicker <dickermoshe@gmail.com>
2023-01-22 18:19:43 -0500

    register user

--- {{cookiecutter.project_slug}}/accounts/admin.py
+++ {{cookiecutter.project_slug}}/accounts/admin.py
@@ -1,11 +1,5 @@
-# accounts/admin.py
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
-
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ["email", "username"]
-
-admin.site.register(CustomUser, CustomUserAdmin)
+# Register your models here.
+from django.contrib.auth import get_user_model
+admin.site.register(get_user_model())
